5d56ccef718b7142695d7a1acc0614976f7a050f
cael brenning <caelbrenning>
2021-11-26 20:30:55 -0600

    fixed dum dum issue :wq

--- commands/hello.js
+++ commands/hello.js
@@ -1,14 +1,14 @@
-// const { SlashCommandBuilder } = require('@discordjs/builders')
+const { SlashCommandBuilder } = require('@discordjs/builders')
 
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName('hello')
-//     .setDescription('says hello'),
-//   async execute(int, c) {
-//     const app = require('../app')
-//     const func = require('../resources/functions')
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('hello')
+    .setDescription('says hello'),
+  async execute(int, c) {
+    const app = require('../app')
+    const func = require('../resources/functions')
 
-//     func.log(`says hello`, int, c)
-//     await int.reply('hello')
-//   },
-// }
+    func.log(`says hello`, int, c)
+    await int.reply('hello')
+  },
+}
