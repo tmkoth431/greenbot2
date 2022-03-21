db5397a4b6536cdf1c3377ed9e4b23209c8afe21
toby <tobykothe@gmail.com>
2022-03-21 16:12:12 -0500

    added assets

--- deploycmd.js
+++ deploycmd.js
@@ -4,20 +4,14 @@ const config = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
-const admincmds = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const adminFiles = fs.readdirSync('./admincmd').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
 }
-for (const file of adminFiles) {
-  const command = require(`./admincmd/${file}`);
-  admincmds.push(command.data.toJSON());
-}
 
-const rest = new REST({ }).setToken(config.token);
+const rest = new REST({}).setToken(config.token);
 
 (async () => {
   try {
@@ -26,10 +20,6 @@ const rest = new REST({ }).setToken(config.token);
     await rest.put(
       Routes.applicationCommands(config.clientId),
       { body: commands },
-    );
-    await rest.put(
-      Routes.applicationGuildCommands(config.clientId, config.guildId),
-      { body: admincmds },
     );
     console.log('Successfully reloaded application (/) commands.')
   } catch (error) {
